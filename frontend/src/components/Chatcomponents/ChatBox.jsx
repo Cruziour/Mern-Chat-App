30e17da0827fe81171bb138df59d377fb164ac18
Rupesh Kumar Raushan <rupeshkumarraushan0@gmail.com>
2025-05-02 20:05:24 +0530

    input layout fix

--- frontend/src/components/Chatcomponents/ChatBox.jsx
+++ frontend/src/components/Chatcomponents/ChatBox.jsx
@@ -325,7 +325,7 @@ const ChatBox = () => {
             <Input
               type="text"
               placeholder="Type a Message...."
-              inputClass="flex-1 border border-gray-300 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-400 w-[70vw]"
+              inputClass="flex-1 border border-gray-300 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-400 w-[70vw] md:w-[55vw] "
               {...register('newMessage')}
               onKeyDown={(e) => {
                 if (e.key === 'Enter') {
